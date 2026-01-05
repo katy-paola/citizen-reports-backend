5938ca136d28e58a9846caf9e39d33d107cd1d71
katy-paola <kbarbozab@unicartagena.edu.co>
2026-01-04 20:58:19 -0500

    fix: update validation messages for title and description in CreateReportDto

--- src/reports/dto/create-report.dto.ts
+++ src/reports/dto/create-report.dto.ts
@@ -3,11 +3,11 @@ import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 export class CreateReportDto {
   @IsString()
   @IsNotEmpty({ message: 'Title is required' })
-  @MinLength(5, { message: 'Title must have at least 5 characters' })
+  @MinLength(5, { message: 'Title must be at least 5 characters' })
   title: string;
 
   @IsString()
   @IsNotEmpty({ message: 'Description is required' })
-  @MinLength(10, { message: 'Description must have at least 10 characters' })
+  @MinLength(10, { message: 'Description must be at least 10 characters' })
   description: string;
 }
